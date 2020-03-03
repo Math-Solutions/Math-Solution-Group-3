55f81464e33db5fe84958f41d894b0d2ab4d772d
emileinerskog <emil@einerskog.se>
2020-03-03 16:15:50 +0100

    Hittade felet till varför det inte funka

--- java/com/example/pa2576/CreateAccount.java
+++ java/com/example/pa2576/CreateAccount.java
@@ -1,5 +1,6 @@
 package com.example.pa2576;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,13 +16,14 @@ public class CreateAccount extends AppCompatActivity {
     EditText createUsername;
     EditText createPassword;
     EditText createPassword2;
-    Button createAccountBtn;
+    Button createAccount;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
 
 
         firstName = findViewById(R.id.createFirstName);
@@ -31,13 +33,13 @@ public class CreateAccount extends AppCompatActivity {
         createPassword = findViewById(R.id.createPassword);
         createPassword2 = findViewById(R.id.createPassword2);
 
-        createAccountBtn = findViewById(R.id.createAccountBtn);
+        createAccount = findViewById(R.id.createAccount);
 
-        createAccountBtn.setOnClickListener(new View.OnClickListener() {
+        createAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                //if( checkIfFilled())
-                   firstName.setText("Det funka");
+
                // openSignInPage();
             }
 
@@ -47,13 +49,12 @@ public class CreateAccount extends AppCompatActivity {
 
 
     }
-    /*
+
     public void openSignInPage(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
     }
-
 
 
     public boolean checkIfFilled(){
@@ -65,24 +66,25 @@ public class CreateAccount extends AppCompatActivity {
 
     }
 
+
     public boolean checkPassword() {
 
 
-        if(createpassword1.getText().toString().length()>=8){
+        if(createPassword.getText().toString().length()>=8){
             boolean checknumber = false;
             boolean checkCapLetter = false;
-            for(int i=0; i<createpassword1.getText().toString().length();i++){
+            for(int i=0; i<createPassword.getText().toString().length();i++){
 
 
-                if(Character.isDigit(createpassword1.getText().toString().charAt(i))){
+                if(Character.isDigit(createPassword.getText().toString().charAt(i))){
                     checknumber = true;
                 }
-                if(Character.isUpperCase(createpassword1.getText().toString().charAt(i))){
+                if(Character.isUpperCase(createPassword.getText().toString().charAt(i))){
                     checkCapLetter = true;
                 }
                 if(checknumber && checkCapLetter){
                     return true;
-            }
+                }
 
             }
         }
@@ -110,5 +112,6 @@ public class CreateAccount extends AppCompatActivity {
         return false;
     }
 
-    */
+
+
 }
