4f8d3c93aa3076db15f2c0f2ba5bceed7eb57eaa
VictoriaJansson <48586822+VictoriaJansson@users.noreply.github.com>
2020-04-08 14:15:03 +0200

    Add files via upload
    This should be the menu

---
+++ MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.navigationdrawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar= findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigationView);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawerOpen,R.string.drawerClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.homepage:
+                Toast.makeText(MainActivity.this, "Homepage selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.profile:
+                Toast.makeText(MainActivity.this, "Profile selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.info:
+                Toast.makeText(MainActivity.this, "Info selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.pic:
+                Toast.makeText(MainActivity.this, "My Solutions was selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rates:
+                Toast.makeText(MainActivity.this, "My ratings was selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                Toast.makeText(MainActivity.this, "Good Bye!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
+    }
+}
+
